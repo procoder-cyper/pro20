31377f9f0c3e4be8cab36492263969c3a2c3fc0e
procoder-cyper <75726865+procoder-cyper@users.noreply.github.com>
2021-07-08 14:34:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var car 
+var wall
+
+
+function setup() {
+  createCanvas(800,400);
+
+  speed = random(55,90)
+  weight = random(400,1200)
+
+ car =  createSprite(400, 200, 50, 50);
+ car.velocityX = speed
+ car.shapeColor = color(255)
+
+ wall = createSprite(1500,200,60,height/2)
+ wall.shapeColor = color(80,80,80)
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(car.isTouching(wall)){
+    car.velocityX = 0
+    deformation = 0.5*weight*speed*speed/22500
+    if(deformation < 100){
+      car.shapeColor = color(0,255,0)
+    }
+    if (deformation < 180&& deformation >100){
+      car.shapeColor = color(230,230,0)
+    }
+  
+    if (deformation > 180){
+      car.shapeColor = color(255,0,0)
+    }
+  }
+  
+  drawSprites();
+}
