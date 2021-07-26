b2ee44f603c5852a033a5fdac51b57f63a1dfabb
procoder-cyper <75726865+procoder-cyper@users.noreply.github.com>
2021-07-26 15:58:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ var wall
 
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1600,400);
 
   speed = random(55,90)
   weight = random(400,1200)
 
- car =  createSprite(400, 200, 50, 50);
+ car =  createSprite(50, 200, 50, 50);
  car.velocityX = speed
  car.shapeColor = color(255)
 
@@ -22,7 +22,7 @@ function draw() {
 
   if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX = 0
-    deformation = 0.5*weight*speed*speed/22500
+   var deformation = 0.5*weight*speed*speed/22500
     if(deformation < 100){
       car.shapeColor = color(0,255,0)
     }
