7e87328e474113dc9d25943b90c14d855308a7c3
procoder-cyper <75726865+procoder-cyper@users.noreply.github.com>
2021-07-26 15:54:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,13 +20,13 @@ function setup() {
 function draw() {
   background(255,255,255);  
 
-  if(car.isTouching(wall)){
+  if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX = 0
     deformation = 0.5*weight*speed*speed/22500
     if(deformation < 100){
       car.shapeColor = color(0,255,0)
     }
-    if (deformation < 180&& deformation >100){
+    if (deformation < 180 && deformation >100){
       car.shapeColor = color(230,230,0)
     }
   
